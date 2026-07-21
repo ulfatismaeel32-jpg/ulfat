71b89d23f59f5c361e31aacf3f140a65bc366151
ulfat isameel <ulfatismaeel32@gmail.com>
2026-07-21 14:58:58 +0500

    add modules

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 export async function musum() {
+        let [karachi, lahore] = ["karachi", "lahore"];
     let [karachiData, lahoreData] = await Promise.all([
         fetch("https://api.open-meteo.com/v1/forecast?latitude=24.86&longitude=67.01&current=weather_code").then(r => r.json()),
         fetch("https://api.open-meteo.com/v1/forecast?latitude=31.55&longitude=74.34&current=weather_code").then(r => r.json())
